8425234d5ea6f898df2efa5332f1e3f2d4989cbb
Giggs471 <blitzwizard45@gmail.com>
2024-04-30 14:23:36 +0600

    region

---
+++ scripts/process.py
@@ -0,0 +1,38 @@
+import openpyxl
+import csv
+from datapackage import Package
+
+def data_package():
+    package = Package()
+    package.infer(r'data/lifeExpectancy-final.csv')
+    package.commit()
+    package.save(r"datapackage.json")
+
+workbook = openpyxl.load_workbook('archive/lifeExpectancy.xlsx')
+worksheet = workbook.active
+desired_columns = [1, 25]
+new_values = ["Region","Value","Year"]
+with open('archive/lifeExpectancy.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(new_values)
+    for row in worksheet.iter_rows(min_row=9, max_row=28):
+            values = [cell.value for cell in row if cell.column in desired_columns]
+            if row[0].row > 6:
+                values.append("2022")
+            writer.writerow(values)
+
+def rename_first_column_cells(input_file, output_file, new_names):
+    with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        # Modify the first cell of each row with new names
+        for new_name, row in zip(new_names, reader):
+            if row:  # Check if row is not empty
+                row[0] = new_name
+                writer.writerow(row)
+input_file = 'archive/lifeExpectancy.csv'
+output_file = 'data/lifeExpectancy-final.csv'
+new_names = ["Region", "Abai Region", "Akmola Region","Aktobe Region","Almaty Region","Atyrau Region","West Kazakhstan Region","Jambyl Region","Jetisu Region","Karaganda Region","Kostanay Region","Kyzylorda Region","Mangystau Region","Pavlodar Region","North Kazakhstan Region","Turkistan Region","Ulytau Region","East Kazakhstan Region","Astana city","Almaty city","Shymkent city"]
+rename_first_column_cells(input_file, output_file, new_names)
+
+data_package();
